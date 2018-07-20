744bc792fe0316c09d17684d3370ac25f32b281b
wanyaxing <wyx@wanyaxing.com>
2018-07-20 17:14:13 +0800

    * 移动布局

--- side.php
+++
@@ -1,6 +0,0 @@
-    <div id="side_list">
-        <div id="user_info">
-            <p>我是万亚星，码农一枚，行业方向：IT互联网。</p>
-            <p>总想写点啥，然而有点忙<del>懒</del>。</p>
-        </div>
-    </div>
--- top.php
+++
@@ -1,6 +0,0 @@
-    <div id="top_bg">
-        <div id="top_nav">
-            <a id="icon_home" href="./">AppJK.com</a>
-            <div id="btn_sideshow">三</div>
-        </div>
-    </div>
