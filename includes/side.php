3005b574861c14ad29a44686c93de0cf01022927
wanyaxing <wyx@wanyaxing.com>
2018-07-20 17:40:17 +0800

    * 移动布局

---
+++ includes/side.php
@@ -0,0 +1,5 @@
+    <div id="side_list">
+        <div id="user_info">
+            <p>A blog which write with markdown which supported by php.</p>
+        </div>
+    </div>
---
+++ includes/top.php
@@ -0,0 +1,6 @@
+    <div id="top_bg">
+        <div id="top_nav">
+            <a id="icon_home" href="./">HOME</a>
+            <div id="btn_sideshow">三</div>
+        </div>
+    </div>
