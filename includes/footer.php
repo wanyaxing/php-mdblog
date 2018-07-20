eb63b1deb756e27008039d77fa45ae374ce8e270
wanyaxing <wyx@wanyaxing.com>
2018-07-20 17:38:02 +0800

    * 移动布局

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<div id="footer_bg">
+    <div id="footer_body">
+        <a href="https://github.com/wanyaxing/php-mdblog/">github.com/wanyaxing/php-mdblog</a>
+    </div>
+</div>
---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+<title><?= $title ?></title>
+<meta charset="utf-8">
+<meta itemprop="name" content="EPIC APPS EVERY DAY">
+<meta itemprop="description" content="the newest apps which free limit in app store.">
+<meta name="viewport" content="width=480,user-scalable=no, viewport-fit=cover">
+<meta name="apple-mobile-web-app-title" content="AppJK">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-touch-fullscreen" content="yes">
+<meta http-equiv="Cache-Control" content="no-siteapp">
+<meta name="format-detection" content="telephone=no">
+<meta name="format-detection" content="email=no">
+<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge,chrome=1">
+<link rel="stylesheet" type="text/css" href="<?=MDBLOG_CDN_URL?>/css/markdown/avenir-white.css">
+<link rel="stylesheet" href="<?=MDBLOG_CDN_URL?>/css/default.css?v=0720.2">
+</head>
