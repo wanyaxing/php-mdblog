f86b6a9a8bc58637fa3ba4accccd463a2e3a279f
wanyaxing <wyx@wanyaxing.com>
2018-07-24 16:36:43 +0800

    * detail 页面不允许本地缓存，避免出现 ajax 和地址栏请求不一致的情况。

--- includes/detail.php
+++ includes/detail.php
@@ -39,6 +39,10 @@
         return $matches[1] . $imgFileUrl . $matches[3] ;
     },$html);
 
+    header("Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0"); // HTTP/1.1
+    header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+    header("Pragma: no-cache"); // Date in the past
+
     if (Utility::isAjax())
     {
         echo $html;
