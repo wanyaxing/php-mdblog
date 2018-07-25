ac661a0fbe4c33d57101e19fc28d5d35de4a9571
wanyaxing <340014824@qq.com>
2018-07-25 18:21:18 +0800

    倒序 sitemap.txt

--- includes/sitemap.txt.php
+++ includes/sitemap.txt.php
@@ -1,11 +1,13 @@
 <?php
-    $dirList   = glob(__dir__.'/../post/*',GLOB_ONLYDIR);
 
     $rootUrl   = (strpos(strtolower($_SERVER['SERVER_PROTOCOL']),'https')  === false ? 'http' : 'https').'://'. $_SERVER['HTTP_HOST'] . preg_replace('/(^.*?\/)sitemap.txt.*/','$1',$_SERVER['REQUEST_URI']);
 
     header("Content-Type: text/plain");
     echo $rootUrl;
     echo "\n";
+
+    $dirList   = glob(__dir__.'/../post/*',GLOB_ONLYDIR);
+    rsort($dirList);
     foreach ($dirList as $dir) {
         $dirInfo = Utility::getDirInfoOfName($dir);
         echo $rootUrl;
