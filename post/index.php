e9f7b435db75b9bc03ea6bcec326c4b02a34a849
wanyaxing <340014824@qq.com>
2018-08-09 07:31:02 +0800

    修复小图片展示仍需要密码的情况

--- post/index.php
+++ post/index.php
@@ -3,17 +3,18 @@
     include(__dir__.'/../includes/config.php');
     include(MDBLOG_ROOT_PATH.'/includes/lib/Utility.php');
 
-    $isAuthed = true;
-    if (defined('PHP_AUTH_USER') && defined('PHP_AUTH_PW') && (!isset($_SERVER['PHP_AUTH_USER'],$_SERVER['PHP_AUTH_PW']) || $_SERVER['PHP_AUTH_USER']!=PHP_AUTH_USER || $_SERVER['PHP_AUTH_PW']!=PHP_AUTH_PW) )
-    {
-        $isAuthed = false;
-    }
+    $isAuthed = false;
 
     $filePath = MDBLOG_ROOT_PATH . urldecode($relativePath);
 
     if (defined('MDBLOG_CDN_MINSIZE') && filesize($filePath) <= MDBLOG_CDN_MINSIZE)
     {
         $isAuthed = true;
+    }
+
+    if (!$isAuthed && defined('PHP_AUTH_USER') && defined('PHP_AUTH_PW') && (!isset($_SERVER['PHP_AUTH_USER'],$_SERVER['PHP_AUTH_PW']) || $_SERVER['PHP_AUTH_USER']!=PHP_AUTH_USER || $_SERVER['PHP_AUTH_PW']!=PHP_AUTH_PW) )
+    {
+        $isAuthed = false;
     }
 
     if (!$isAuthed)
