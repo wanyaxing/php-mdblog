a3f6d6516913a9b13c307138a1b5fa54f9c0af41
wanyaxing <340014824@qq.com>
2018-08-08 18:07:49 +0800

    修复 post 目录直接访问使用了错误的认证密码的问题

--- post/index.php
+++ post/index.php
@@ -3,7 +3,7 @@
     include(__dir__.'/../includes/config.php');
     include(MDBLOG_ROOT_PATH.'/includes/lib/Utility.php');
 
-    if (defined('PHP_AUTH_USER') && defined('PHP_AUTH_PW') && (!isset($_SERVER['PHP_AUTH_USER'],$_SERVER['PHP_AUTH_PW']) || $_SERVER['PHP_AUTH_USER']!='admin' || $_SERVER['PHP_AUTH_PW']!='123456') )
+    if (defined('PHP_AUTH_USER') && defined('PHP_AUTH_PW') && (!isset($_SERVER['PHP_AUTH_USER'],$_SERVER['PHP_AUTH_PW']) || $_SERVER['PHP_AUTH_USER']!=PHP_AUTH_USER || $_SERVER['PHP_AUTH_PW']!=PHP_AUTH_PW) )
     {
         header('WWW-Authenticate: Basic realm="文件区数据需要用户认证后才可查看。"');
         header('HTTP/1.0 401 Unauthorized');
