1ecb59b418b3aa92573d724c317fededbe7f828d
wanyaxing <wanyaxing@julanling.com>
2019-09-25 19:00:57 +0800

    修复第三页之后链接携带is_ajax参数的问题

--- includes/list.php
+++ includes/list.php
@@ -24,9 +24,14 @@ if (!is_null($tag)) {
 <?php if (count($dirListInfo['currentList'])==0): ?>
         <a class="btn_loadmore" >你发现了一片荒漠，这儿啥都没有。。。</a>
 <?php endif ?>
-<?php if ($page * $size < $dirListInfo['countTotal']): ?>
-        <a class="btn_loadmore" href="<?= './?'.http_build_query(array_merge($_GET, array('page'=>$page+1))) ?>">点我加载更多</a>
-<?php endif ?>
+<?php if ($page * $size < $dirListInfo['countTotal']) {
+    $params = array_merge($_GET, array('page'=>$page+1));
+    if (isset($params['is_ajax'])) {
+        unset($params['is_ajax']);
+    }
+    printf('<a class="btn_loadmore" href="./?%s">点我加载更多</a>', http_build_query($params));
+} 
+?>
 <?php if (!MDBLOG_IS_AJAX): ?>
     </main>
 <?php include __dir__ . '/side.php'; ?>
